00fa192c6b874ee161f43a121804340a37ff9dfb
Dmitrii <d.n.baklai@gmail.com>
2023-03-11 14:12:23 +0200

    dfgf

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+  title: 'Helpdesk docs',
+  description: 'A Helpdesk documentation',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }]
+  }
+});
