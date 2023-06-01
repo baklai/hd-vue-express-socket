a52dad4d437533b0cdfe6382ad700cb6c5951f75
Dmitrii <d.n.baklai@gmail.com>
2023-06-01 15:13:17 +0300

    fix

---
+++ agent/HDAgentService/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Text;
+
+namespace HDAgentService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new HDService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
